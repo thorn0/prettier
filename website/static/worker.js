b55181b17c71bc1f4c6b87cae8f6120e1a5e9d83
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2021-02-16 15:42:01 +0200

    Make error serialization more cross-browser

--- website/static/worker.js
+++ website/static/worker.js
@@ -168,6 +168,19 @@ function formatCode(text, options) {
     }
     // Likely a bug in Prettier
     // Provide the whole stack for debugging
-    return { formatted: e.stack || String(e), error: true };
+    return { formatted: stringifyError(e), error: true };
   }
+}
+
+function stringifyError(e) {
+  const stringified = String(e);
+  if (typeof e.stack !== "string") {
+    return stringified;
+  }
+  if (e.stack.includes(stringified)) {
+    // Chrome
+    return e.stack;
+  }
+  // Firefox
+  return stringified + "\n" + e.stack;
 }
